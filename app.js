2bae20e18ee09e03214f9fab761896e40340f914
AJ ONeal <awesome@coolaj86.com>
2015-08-07 12:10:17 -0600

    add logger and stuff

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ module.exports.create = function () {
   var express = require('express');
   var app = express();
   var staticServer = express.static(__dirname + '/public');
+  var bodyParser = require('body-parser');
 
 
   function handler(req, res) {
-    var result = '';
-    req.on('data', function (buffer) {
-      console.log(buffer);
-      console.log(buffer.toString('utf8'));
-      result += buffer.toString('utf8');
-    });
-    req.on('end', function () {
-      console.log('done', result);
-      res.statusCode = 200;
-      res.write(JSON.stringify({ success: true }));
-      res.end();
-    });
+    console.log(req.body);
+    res.setHeader('X-Foo-Header', 'value');
+
+    res.send(req.body);
   }
 
-  app.use('/', staticServer);
+  function logger(req, res, next) {
+    console.log(req.method, req.url);
+    req.logged = true;
+    next();
+  }
 
-  app.get('/api/messages', handler);
+  function getMessage(req, res) {
+    var name = req.params.name || 'friend';
+    res.send({ message: "hello " + name });
+  }
+
+  app.use('/', logger);
+  app.use('/', staticServer);
+  app.use('/api', bodyParser.json()); // good 
+  // badyParser.urlencoded() // CSRF
+
+  app.get('/api/messages/:name', getMessage);
   app.post('/api/messages', handler);
   app.delete('/api/messages', handler);
 
